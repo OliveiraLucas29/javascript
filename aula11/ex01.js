28b995ac795e2cb1b4dbe358e580f36fb6e35dea
N0b0dy29 <oliveiraluke29@gmail.com>
2022-09-21 18:15:50 -0300

    condições

---
+++ aula11/ex01.js
@@ -0,0 +1,5 @@
+var vel = 62
+console.log(`Você está dirigindo à ${vel}Km/h`) 
+if (vel > 60) { //Condição simples
+    console.log (`Você está acima da velocidade máxima permitida. MULTADO!`)
+}
---
+++ aula11/ex02.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Você está no país: ${pais}`)
+if (pais == 'Brasil') {  //Condição composta (if e else)
+    console.log(`Nativo`)
+} else {
+    console.log(`Estrangeiro`)
+}
